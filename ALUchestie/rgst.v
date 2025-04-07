0e2baa9e64a3fe326a7f4c41dc2676ea0921a0a8
Ale <oprtitamariaalexia@gmail.com>
2025-04-07 16:30:54 +0300

    mux and d_ff modules

--- ALUchestie/rgst.v
+++ ALUchestie/rgst.v
@@ -1,3 +1,48 @@
+module mux4 (
+    input [1:0] sel,
+    input a, b, c, d,
+    output out
+);
+    assign out = (sel == 2'b00) ? a :
+                 (sel == 2'b01) ? b :
+                 (sel == 2'b10) ? c :
+                                  d;
+endmodule
+
+/*
+module mux4_tb;
+    reg        a, b, c, d;
+    reg [1:0]  sel;
+    wire       out;
+    mux4 dut (
+        .a(a),
+        .b(b),
+        .c(c),
+        .d(d),
+        .sel(sel),
+        .out(out)
+    );
+    integer k;
+    initial begin
+        $display(" a  b  c  d | sel | out ");
+	$monitor(" %b  %b  %b  %b | %02b  |  %b", a, b, c, d, sel, out);
+        for (k = 0; k < 16; k = k + 1) begin
+            {d, c, b, a} = $urandom(); 
+            sel = $urandom();        
+            #10; 
+        end
+    end
+endmodule*/
+
+module d_ff(
+  input clk,rst,d,
+  output reg q
+);
+  always @ (posedge clk or posedge rst)        
+    if (rst)      q<=0;
+    else          q<=d;
+endmodule
+
 module rgst (
     input clk, rst, ld, clr,
     input [7:0] d, 
