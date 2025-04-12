2e0c556abd9bda7fce6fd6ba60c3e7829dea8108
Alexrupenetu <alextuneaa@gmail.com>
2025-04-12 16:41:44 +0300

    Parametrizat registru

--- ALUchestie/ushift.v
+++ ALUchestie/ushift.v
@@ -1,43 +1,55 @@
 `include"mux.v"
 `include"dff.v"
-module ushift(
-	input[7:0]a,
-	input[1:0] sel,
-	input bsRight, bsLeft, reset, clk,
-	output wire[7:0]a_shifted
+module ushift #(
+    parameter WIDTH = 8
+)(
+    input [WIDTH-1:0] a,
+    input [1:0] sel,
+    input bsRight, bsLeft, reset, clk,
+    output wire [WIDTH-1:0] a_shifted
 );
 
-wire out_mux[7:0];
+    wire [WIDTH-1:0] out_mux;
 
-mux mux0(.sel(sel), .d({a[0], bsLeft, a_shifted[1], a_shifted[0]}), .out(out_mux[0]));
-mux mux1(.sel(sel), .d({a[1], a_shifted[0], a_shifted[2], a_shifted[1]}), .out(out_mux[1]));
-mux mux2(.sel(sel), .d({a[2], a_shifted[1], a_shifted[3], a_shifted[2]}), .out(out_mux[2]));
-mux mux3(.sel(sel), .d({a[3], a_shifted[2], a_shifted[4], a_shifted[3]}), .out(out_mux[3]));
-mux mux4(.sel(sel), .d({a[4], a_shifted[3], a_shifted[5], a_shifted[4]}), .out(out_mux[4]));
-mux mux5(.sel(sel), .d({a[5], a_shifted[4], a_shifted[6], a_shifted[5]}), .out(out_mux[5]));
-mux mux6(.sel(sel), .d({a[6], a_shifted[5], a_shifted[7], a_shifted[6]}), .out(out_mux[6]));
-mux mux7(.sel(sel), .d({a[7],a_shifted[6], bsRight, a_shifted[7]}), .out(out_mux[7]));
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin : shift_logic
+            wire d0, d1, d2, d3;
 
-dff dff0(.clk(clk), .reset(reset), .d(out_mux[0]), .q(a_shifted[0]));
-dff dff1(.clk(clk), .reset(reset), .d(out_mux[1]), .q(a_shifted[1]));
-dff dff2(.clk(clk), .reset(reset), .d(out_mux[2]), .q(a_shifted[2]));
-dff dff3(.clk(clk), .reset(reset), .d(out_mux[3]), .q(a_shifted[3]));
-dff dff4(.clk(clk), .reset(reset), .d(out_mux[4]), .q(a_shifted[4]));
-dff dff5(.clk(clk), .reset(reset), .d(out_mux[5]), .q(a_shifted[5]));
-dff dff6(.clk(clk), .reset(reset), .d(out_mux[6]), .q(a_shifted[6]));
-dff dff7(.clk(clk), .reset(reset), .d(out_mux[7]), .q(a_shifted[7]));
+            assign d0 = a[i];
+            assign d1 = (i == 0) ? bsLeft : a_shifted[i - 1];
+            assign d2 = (i == WIDTH - 1) ? bsRight : a_shifted[i + 1];
+            assign d3 = a_shifted[i];
+
+            mux m (
+                .sel(sel),
+                .d({d0, d1, d2, d3}),
+                .out(out_mux[i])
+            );
+
+            dff ff (
+                .clk(clk),
+                .reset(reset),
+                .d(out_mux[i]),
+                .q(a_shifted[i])
+            );
+        end
+    endgenerate
 
 endmodule
 
+
+   
 module ushift_tb;
 
-  reg [7:0] a;
+  parameter WIDTH = 8;
+
+  reg [WIDTH-1:0] a;
   reg [1:0] sel;
   reg bsRight, bsLeft, reset, clk;
-  wire [7:0] a_shifted;
+  wire [WIDTH-1:0] a_shifted;
 
-  // Instan?ierea modulului de testat
-  ushift uut (
+  ushift #(.WIDTH(WIDTH)) uut (
     .a(a),
     .sel(sel),
     .bsRight(bsRight),
@@ -47,7 +59,6 @@ module ushift_tb;
     .a_shifted(a_shifted)
   );
 
-  // Clock generator
   always #5 clk = ~clk;
 
   initial begin
@@ -57,38 +68,34 @@ module ushift_tb;
     clk = 0;
     reset = 1;
     a = 8'b10110011;
-    sel = 2'b11;  // Load value
+    sel = 2'b11;  // Load
     bsRight = 1'b1;
     bsLeft = 1'b0;
 
-    #10 reset = 0;  // Scoatem din reset
+    #10 reset = 0;
     #10;
 
-    // Test?m men?inerea valorii (00)
-    sel = 2'b00;
+    sel = 2'b00;  // No change
     #10;
 
-    // Shift stânga (01)
-    sel = 2'b01;
+    sel = 2'b01;  // Shift left
     bsLeft = 1'b1;
     #10;
 
-    // Shift dreapta (10)
-    sel = 2'b10;
+    sel = 2'b10;  // Shift right
     bsRight = 1'b0;
     #10;
 
-    // Reînc?rcare
-    sel = 2'b11;
+    sel = 2'b11;  // Reload
     a = 8'b11110000;
     #10;
 
-    // Înc? un shift la stânga
-    sel = 2'b01;
+    sel = 2'b01;  // Shift left again
     bsLeft = 1'b1;
     #10;
 
     $display("Test completed.");
+   
   end
 
   initial begin
